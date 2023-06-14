01f63d7a1594e8217c63785e7021894c6ac140e6
sanz17 <kundusanhita13@gmail.com>
2023-06-14 12:07:51 +0530

    tdd added

--- util.py
+++ util.py
@@ -1,3 +1,3 @@
 def add_years_to_date(original_date,years_add):
-    result=original_date.replace(year=original_date)
+    result=original_date.replace(year=original_date+years_add)
     return result
